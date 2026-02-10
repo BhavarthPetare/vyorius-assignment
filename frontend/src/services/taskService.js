21b866cfbaa02376512f80e0e66ce9aeb6580108
BhavarthPetare <158989218+BhavarthPetare@users.noreply.github.com>
2026-02-10 11:29:29 +0530

    Update taskService.js

--- frontend/src/services/taskService.js
+++ frontend/src/services/taskService.js
@@ -1,1 +1,21 @@
-hsjdjd
+import { socket } from "../socket";
+
+// CREATE
+export const createTask = (task) => {
+    socket.emit("task:create", task);
+};
+
+// UPDATE
+export const updateTask = (task) => {
+    socket.emit("task:update", task);
+};
+
+// MOVE
+export const moveTask = (id, status) => {
+    socket.emit("task:move", { id, status });
+};
+
+// DELETE
+export const deleteTask = (id) => {
+    socket.emit("task:delete", id);
+};
