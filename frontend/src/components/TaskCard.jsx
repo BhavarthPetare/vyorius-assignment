c9102c8ae80545600325629481fb659ac5dcef1c
BhavarthPetare <158989218+BhavarthPetare@users.noreply.github.com>
2026-02-13 12:35:28 +0530

    Update TaskCard.jsx

--- frontend/src/components/TaskCard.jsx
+++ frontend/src/components/TaskCard.jsx
@@ -1,10 +1,14 @@
+import { useState } from "react";
 import { useDrag } from "react-dnd";
 
 const ItemTypes = {
     TASK: "task",
 };
 
-function TaskCard({ task, onDelete }) {
+function TaskCard({ task, onDelete, onUpdate }) {
+
+    const [isEditing, setIsEditing] = useState(false);
+    const [editData, setEditData] = useState(task);
 
     const [{ isDragging }, drag] = useDrag(() => ({
         type: ItemTypes.TASK,
@@ -14,25 +18,54 @@ function TaskCard({ task, onDelete }) {
         }),
     }));
 
+    const handleSave = () => {
+        onUpdate(editData);
+        setIsEditing(false);
+    };
+
     return (
         <div
             ref={drag}
             style={{
                 border: "1px solid #999",
-                padding: "8px",
-                marginBottom: "8px",
+                padding: "10px",
+                marginBottom: "10px",
                 backgroundColor: "white",
                 opacity: isDragging ? 0.5 : 1,
                 cursor: "move",
             }}
         >
-            <strong>{task.title}</strong>
-            <br />
-            <small>{task.priority}</small>
-            <br />
-            <button onClick={() => onDelete(task.id)}>
-                Delete
-            </button>
+            {isEditing ? (
+                <>
+                    <input
+                        value={editData.title}
+                        onChange={(e) =>
+                            setEditData({ ...editData, title: e.target.value })
+                        }
+                    />
+                    <select
+                        value={editData.priority}
+                        onChange={(e) =>
+                            setEditData({ ...editData, priority: e.target.value })
+                        }
+                    >
+                        <option>Low</option>
+                        <option>Medium</option>
+                        <option>High</option>
+                    </select>
+
+                    <button onClick={handleSave}>Save</button>
+                </>
+            ) : (
+                <>
+                    <strong>{task.title}</strong>
+                    <br />
+                    <small>{task.priority}</small>
+                    <br />
+                    <button onClick={() => setIsEditing(true)}>Edit</button>
+                    <button onClick={() => onDelete(task.id)}>Delete</button>
+                </>
+            )}
         </div>
     );
 }
